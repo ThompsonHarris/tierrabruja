2b2c6621ff9d188437eef2e235e5b147a05f4248
Thompson Harris <william.thompson.harris@gmail.com>
2020-07-24 11:30:20 -0400

    added logo to splash

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './app.scss';
+//components
+import Logo from './components/Icons/Logo.jsx';
 
 class App extends React.Component {
   render() {
-    return <div className={'Main'}></div>;
+    return (
+      <div className='Main'>
+        <Logo className='Main_logo' />
+      </div>
+    );
   }
 }
 
