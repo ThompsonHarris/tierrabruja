baa159e8130957516ca25dfb61addbcd71117191
Thompson Harris <william.thompson.harris@gmail.com>
2020-12-06 14:52:28 -0500

    added simple portfolio view

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {Link} from 'react-router-dom'
 //components
 import Logo from '../../components/Icons/Logo.jsx';
 //actions
@@ -9,7 +10,7 @@ class Home extends React.Component {
   render() {
     return (
       <>
-        <div className='container mx-auto flex flex-col justify-center'>
+        <div className='container mx-auto flex flex-col justify-center h-screen'>
           <Logo className='fill-current cus-blue' />
           <a
             className='text-center cursor-pointer font-bold cus-blue animate-bounce'
@@ -17,12 +18,7 @@ class Home extends React.Component {
           >
             summon us
           </a>
-          <a
-            className='text-center cursor-pointer font-bold cus-blue animate-bounce'
-            onClick={() => this.props.navDialogueMenu('login')}
-          >
-            login
-          </a>
+          <Link to='/portfolio' className={'text-center cursor-pointer font-bold cus-blue animate-bounce'}>portfolio</Link>
         </div>
       </>
     );
---
+++ src/pages/portfolio/portfolio.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+class Portfolio extends React.Component{
+  render(){
+    return (
+      <div className='lg:w-11/12 md:w-5/6 w-4/6 flex flex-row justify-center'>
+        <div className='items-center flex flex-col justify-start w-full'>
+        <div className='text-center font-bold cus-blue animate-bounce mt-2'>** images below **</div>
+        {
+          this.props.images.map((img)=>{
+            return (
+              <img src={img.fullImage} className='w-full object-cover my-8'/>
+            )
+          })
+        }
+
+        </div>
+      </div>
+    )
+}
+}
+
+const mapStateToProps = (state) => ({
+  images:state.general.images
+})
+
+export default connect(mapStateToProps, null)(Portfolio)
