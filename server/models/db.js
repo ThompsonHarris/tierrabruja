b08784009d3a27dc9cf47fb9fd57268cdd4e6190
Thompson Harris <william.thompson.harris@gmail.com>
2021-03-24 21:10:24 -0400

    added config

--- server/models/db.js
+++ server/models/db.js
@@ -1,10 +1,30 @@
 const sequelize = require('sequelize');
 
+let config;
+if (process.env.DATABASE_URL) {
+  config = {
+    logging: false,
+    operatorsAliases: false,
+    dialect: 'postgres',
+    protocol: 'postgres',
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  };
+} else {
+  config = {
+    logging: false,
+    operatorsAliases: false,
+  };
+}
+
 const db = new sequelize(
   process.env.DATABASE_URL || 'postgres://localhost:5432/tierrabruja',
-  {
-    logging: false,
-  }
+  config
 );
 
 module.exports = db;
